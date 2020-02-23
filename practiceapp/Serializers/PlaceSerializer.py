0f38eb00c83497d2809b77503730861cc0e0a526
hitanshi <hitanshimehta18@gmail.com>
2020-02-23 17:56:36 +0530

    Added comment

--- practiceapp/Serializers/PlaceSerializer.py
+++ practiceapp/Serializers/PlaceSerializer.py
@@ -34,6 +34,7 @@ class PlaceSerializer(serializers.ModelSerializer):
         ]
         fields = ('id','name','address')
 
+        #create with manager
         def create(self,validate_data):
             print("validate_data",validate_data)
             return Place.objects.create(**validate_data)
