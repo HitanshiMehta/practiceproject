4c1e5736ce8a92525f86481cea735390f1e76d2d
hitanshi <hitanshimehta18@gmail.com>
2020-02-23 20:43:28 +0530

    workspace

--- practiceapp/Serializers/PlaceSerializer.py
+++ practiceapp/Serializers/PlaceSerializer.py
@@ -23,7 +23,7 @@ class PlaceSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("hitanshiii ma be i kem che?")
         return  data
 
-
+    #Just to learn how to merge
     class Meta:
         model= Place
         validators = [
